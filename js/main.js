848224423e6fbcdf95a4d307a28a1d7a9f787c7c
paulnieu <paulnieu@yahoo.com>
2018-07-31 20:45:51 -0700

    fixed property attributes and JS

--- js/main.js
+++ js/main.js
@@ -2,93 +2,93 @@ var zodiac = [
   {
     sign:"Aquarius",
     dates:"January 20 - February 18",
-    traits:"deep,imaginative,original and uncompromising",
-    image:""
+    traits:"Deep,imaginative,original and uncompromising",
+    image:"img/aquarius.jpeg"
   },
   {
     sign:"Pisces",
     dates:"February 19 - March 20",
-    traits:"affectionative, empathetic, wise and artistic",
-    image:""
+    traits:"Affectionative, empathetic, wise and artistic",
+    image:"img/pisces.jpg"
   },
   {
     sign:"Aries",
     dates:"March 21 - April 19",
-    traits:"eager, dynamic, quick and competitive",
-    image:""
+    traits:"Eager, dynamic, quick and competitive",
+    image:"img/aries.jpeg"
   },
   {
     sign:"Taurus",
     dates:"April 20 - May 18",
-    traits:"strong, dependable, sensual and creative",
-    image:""
+    traits:"Strong, dependable, sensual and creative",
+    image:"img/taurus.jpg"
   },
   {
     sign:"Gemini",
     dates:"May 21 - June 20",
-    traits:"versatile, expressive, curious and kind ",
-    image:""
+    traits:"Versatile, expressive, curious and kind ",
+    image:"img/gemini.jpg"
   },
   {
     sign:"Cancer",
     dates:"June 21 - July 22",
-    traits:"intuitive, sentimental, compassionate and protective",
-    image:""
+    traits:"Intuitive, sentimental, compassionate and protective",
+    image:"img/cancer.jpg"
   },
   {
     sign:"Leo",
     dates:"July 23 - August 22",
-    traits:"dramatic, outgoing, fiery and self-assured",
-    image:""
+    traits:"Dramatic, outgoing, fiery and self-assured",
+    image:"img/leo.jpg"
   },
   {
     sign:"Virgo",
     dates:"August 23 - September 22",
-    traits:"deep,imaginative,original and uncompromising",
-    image:""
+    traits:"Deep,imaginative,original and uncompromising",
+    image:"img/virgo.jpeg"
   },
   {
     sign:"Libra",
     dates: "September 23 - October 22",
-    traits:"social, fair-minded, diplomatic and gracious",
-    image:""
+    traits:"Social, fair-minded, diplomatic and gracious",
+    image:"img/libra.jpg"
   },
   {
     sign:"Scorpio",
     dates:"October 23 - November 21",
-    traits:"passionate, stubborn, resourceful and brave",
-    image:""
+    traits:"Passionate, stubborn, resourceful and brave",
+    image:"img/scorpio.jpg"
   },
   {
     sign:"Sagittarius",
     dates:"November 22 - December 21",
-    traits:"extroverted, optimistic, funny and generous",
-    image:""
+    traits:"Extroverted, optimistic, funny and generous",
+    image:"img/saggitarious.jpg"
   },
   {
     sign:"Capricorn",
     dates:"December 22 - January 19",
-    traits:"serious, independent, disciplined, and tenacious",
-    image:""
+    traits:"Serious, independent, disciplined, and tenacious",
+    image:"img/capricorn.jpg"
   }
 ];
 
 var input = document.getElementById("zodiacName");
-var myH1 = document.getElementById("getZodiac");
+var myH1 = document.getElementById("zodiac");
 var myH3 = document.getElementById("date");
-var myP = document.getElementById("trait");
+var myP = document.getElementById("traits");
 var myImg = document.getElementById("zodiacImg");
 
 
 function getZodiac() {
   for(var i = 0; i < zodiac.length; i ++){
-    if(input.value == zodiac[i].name) {
-      myH1.innerText = zodiac[i].name;
+    if(input.value.toLowerCase() == zodiac[i].sign.toLowerCase()) {
+      myH1.innerText = zodiac[i].sign;
       myH3.innerText = zodiac[i].dates;
-      myP.innerText = Zodiac[i].traits;
+      myP.innerText = zodiac[i].traits;
+      myImg.src = zodiac[i].image;
 
       return;
     }
   }
-
 }
